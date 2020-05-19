1cfda8292fb4c3ed918d9305de0ffe51043d8502
Brian Beach <coder@beachfamily.net>
2020-05-18 18:03:51 -1000

    Add timeout to image test.

--- test-image.sh
+++ test-image.sh
@@ -5,14 +5,16 @@
 # calls the web server inside it as a quick smoke test.
 #
 
+set -e
+
 container_id=$(docker run -d -p 8000:8888 $1)
 echo "Container is: ${container_id}"
 
 # The server is not ready instantly, so we have to wait
-# until it is
-while true; do
-  status=$(curl -s http://localhost:8000/status)
-  if [[ ! -z "$(curl -s http://localhost:8000/status)" ]]; then
+# until it is.  Wait up to 10 seconds.
+for n in $(seq 10); do
+  echo "Attempt ${n} to get status"
+  if status=$(curl -s http://localhost:8000/status); then
     break
   fi
   sleep 1
