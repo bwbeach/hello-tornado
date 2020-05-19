c3783548a1ea21aa495a5395818b75f82dc419bc
Brian Beach <coder@beachfamily.net>
2020-05-18 17:32:23 -1000

    Add signal handling for shutdown.
    This will shut down the server cleanly, so docker
    won't have to wait 10 seconds.

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,4 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-
 ENTRYPOINT ["/usr/src/app/hello.py"]
--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import signal
 import tornado.ioloop
 import tornado.web
 import unittest
+
+PORT = 8888
 
 class DummyTest(unittest.TestCase):
     def test_it(self):
@@ -20,7 +23,31 @@ def make_app():
     ])
 
 
-if __name__ == "__main__":
+def main():
     app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    server = app.listen(PORT)
+    ioloop = tornado.ioloop.IOLoop.current()
+
+    def stop():
+        # Stop listening for more connections, and deregister
+        # from the ioloop.
+        server.stop()
+        # Stop the ioloop.  Running tasks may still fining.
+        ioloop.stop()
+
+    def handler(_signum, _stack_frame):
+        # Tell the ioloop to run the stop action.  IO loops are
+        # not very thread safe, and we can't run the stop()
+        # method in the signal handler.
+        ioloop.add_callback_from_signal(stop)
+
+    signal.signal(signal.SIGHUP, handler)
+    signal.signal(signal.SIGTERM, handler)
+
+    print('listening on port', PORT)
+    ioloop.start()
+    print('server stopped')
+
+
+if __name__ == "__main__":
+    main()
